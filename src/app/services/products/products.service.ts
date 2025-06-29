b3d67dfc6a0f494581b9fe43f742b2b499dcb720
Rafaela Andrade <rafaelaandrade.costa@gmail.com>
2025-06-29 18:25:33 -0300

    T06
    Implementação do service de busca de produtos.
    Hot observables e memory leak.

---
+++ src/app/services/products/products.service.ts
@@ -0,0 +1,34 @@
+import { GetAllProductsResponse } from './../../models/interfaces/products/response/GetAllProductsResponse';
+import { CookieService } from 'ngx-cookie-service';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  private API_URL = environment.API_URL;
+  private JWT_TOKEN = this.Cookie.get('USER_INFO');
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.JWT_TOKEN}`
+    }),
+  };
+
+  constructor(
+    private http: HttpClient,
+    private Cookie: CookieService
+  ) { }
+
+  GetAllProducts(): Observable<Array<GetAllProductsResponse>> {
+    return this.http.get<Array<GetAllProductsResponse>>(
+      `${this.API_URL}/products`,
+      this.httpOptions
+    )
+      .pipe(
+        map((product) => product.filter((data) => data?.amount > 0)));
+  }
+}
