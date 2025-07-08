669ac5c3938aca87190ef6115f195a0c4058713f
Rafaela Andrade <rafaelaandrade.costa@gmail.com>
2025-07-07 22:16:27 -0300

    T12
    Módulo, componente e rota de categorias. Implementação componente de tabela de categorias.
    Funcionalidade de delete da categoria.

---
+++ src/app/modules/categories/categories.routing.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { CategoriesHomeComponent } from "./page/categories-home/categories-home.component";
+import { Routes } from "@angular/router";
+
+export const CATEGORIES_ROUTES: Routes = [
+  {
+    path: '',
+    component: CategoriesHomeComponent,
+  }
+]
